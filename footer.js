81d85b9750334b4da878fe084db72f5b8319c161
churchmouse <churchillomosh@gmail.com>
2016-09-13 18:08:56 +0300

    The footer of the app

--- footer.js
+++ footer.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component{
     render(){
         return(
             <div className="footer">
-                <div id="copyright">2016 Tudu.com</div>
+                <div id="copyright">2016 Todo.com</div>
             </div>
         );
     }
